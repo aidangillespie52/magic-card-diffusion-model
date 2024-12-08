f6d1ad7b9bdc1afe2ae911a171a86e85af30332f
Aidan Gillespie <aidangillespie52@gmail.com>
2024-12-07 21:11:29 -0500

    Added test functionality so you can test a random image on it and it display the image and the output.

--- models/dae.py
+++ models/dae.py
@@ -1,5 +1,9 @@
 from torch import nn
-
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+ 
 class DenoisingAutoencoder(nn.Module):
     def __init__(self):
         super(DenoisingAutoencoder, self).__init__()
@@ -28,3 +32,31 @@ class DenoisingAutoencoder(nn.Module):
         x = self.encoder(x)
         x = self.decoder(x)
         return x
+
+    def test(self, img):
+        img = img.to(device)
+
+        pred = self.forward(img.unsqueeze(0))[0].to(device)
+        
+        pred_img = pred.permute(1, 2, 0).detach().cpu().numpy()
+        
+        pred_img = np.clip(pred_img, 0, 1)
+
+        act_img = img.permute(1, 2, 0).cpu().numpy()
+        
+        _, axes = plt.subplots(1, 2, figsize=(7, 7))
+
+        axes[0].imshow(act_img)
+        axes[0].set_title('Actual Image')
+        axes[0].axis('off')
+
+        axes[1].imshow(pred_img)
+        axes[1].set_title('Enchanced Image')
+        axes[1].axis('off')
+
+        # Adjust layout to avoid overlap
+        plt.tight_layout()
+
+        plt.show()
+
+        return pred_img
